573da9ca69f9b02a0704beab1d0bb3abe78877c7
Drew Broadley <drew.broadley@gmail.com>
2016-05-17 18:50:27 +1200

    First release
    It’s not perfect, but who cares ? :D

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,40 +2,44 @@
 
 var gulp = require('gulp');
 var babel = require('gulp-babel');
-var sass = require('gulp-sass');
+var less = require('gulp-less');
 const imagemin = require('gulp-imagemin');
 let src = 'src';
 let dist = 'dist';
+var paths = {
+  less: ['./src/less/*.less']
+};
 
-gulp.task('default', ['babel', 'sass','images', 'move', 'webfont']);
+gulp.task('default', ['babel', 'less','images', 'vendor']);
 
 gulp.task('babel', function () {
   return gulp.src(`${src}/*.js`)
       .pipe(babel())
-      .pipe(gulp.dest(`${dist}`));
+      .pipe(gulp.dest('assets/js'));
 });
 
-gulp.task('sass', function () {
-  return gulp.src('src/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist'));
+gulp.task('less', function () {
+  return gulp.src([
+    'src/less/style.less'
+  ], {base: 'src/less'})
+    .pipe(less())
+    .pipe(gulp.dest('assets/css'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch(paths.less, ['less']);
+});
+
+
+
 gulp.task('images', function(){
   return gulp.src('src/images/**/*')
       .pipe(imagemin())
-      .pipe(gulp.dest('dist/images'))
+      .pipe(gulp.dest('assets/images'))
 })
-gulp.task('move', function() {
+gulp.task('vendor', function() {
   return gulp.src([
-    'src/index.html',
-    'src/downloads/**/*'
-  ], {base: 'src'})
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('webfont', function() {
-  return gulp.src([
-    'node_modules/clear-sans-webfont/fonts/**/*'
-  ], {base: 'node_modules/clear-sans-webfont'})
-    .pipe(gulp.dest('dist'));
+    'vendor/bower/bootstrap/dist/*/*'
+  ], {base: 'vendor/bower'})
+    .pipe(gulp.dest('assets/vendor'));
 });
